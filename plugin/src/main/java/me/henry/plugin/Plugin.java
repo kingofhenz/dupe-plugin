6463dfa45fd05a8cf02e36814330edcfaf4ed027
kingofhenz <93614808+kingofhenz@users.noreply.github.com>
2022-07-31 14:54:41 -0400

    Update Plugin.java

--- plugin/src/main/java/me/henry/plugin/Plugin.java
+++ plugin/src/main/java/me/henry/plugin/Plugin.java
@@ -7,24 +7,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.HumanEntity;
 
 public final class Plugin extends JavaPlugin implements Listener {
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-        System.out.println("helo");
-        getServer().getPluginManager().registerEvents(this, this);
-    }
-
-    public void onPlayerJoin(PlayerJoinEvent event){
-
-        event.setJoinMessage("fag");
-
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-        System.out.println("bye");
-    }
+ @Override
+    public boolean onCommand(CommandSender sender,Command cmd,String label,  String[] args) {
+        if (sender instanceof Player){
+            Player player = (Player) sender;
+            
+        }
 }
