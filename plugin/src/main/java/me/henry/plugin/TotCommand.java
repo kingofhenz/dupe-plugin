f70c85d2abe34fc59dd9aaebf1984de71aff709c
kingofhenz <93614808+kingofhenz@users.noreply.github.com>
2022-07-31 14:55:47 -0400

    Update TotCommand.java

--- plugin/src/main/java/me/henry/plugin/TotCommand.java
+++ plugin/src/main/java/me/henry/plugin/TotCommand.java
@@ -1,11 +1,3 @@
-package me.henry.plugin;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-
 public class TotCommands implements CommandExecutor {
 
     @Override
@@ -14,11 +6,26 @@ public class TotCommands implements CommandExecutor {
             Player player = (Player) sender;
         }
         if (cmd.getName().equalsIgnoreCase("heal")){
-           
+       
+            if (sender instanceof Player){
+                Player player = (Player) sender;
+                player.setHealth(20);
+            }
 
         }
         if (cmd.getName().equalsIgnoreCase("dupe")){
+         
+            if (sender instanceof Player){
+                Player player = (Player) sender;
+                player.getInventory().addItem(player.getItemInHand());
+            }
 
+            if (cmd.getName().equalsIgnoreCase("feed")){
+                if (sender instanceof Player){
+                    Player player = (Player) sender;
+                    player.setSaturation(20);
+                }
+            }
         }
         return true;
     }
