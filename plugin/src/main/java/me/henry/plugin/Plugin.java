eafe358d4f0a8233948db8d5c19dac8f5dc1a471
kingofhenz <93614808+kingofhenz@users.noreply.github.com>
2022-07-31 14:50:26 -0400

    Update Plugin.java

--- plugin/src/main/java/me/henry/plugin/Plugin.java
+++ plugin/src/main/java/me/henry/plugin/Plugin.java
@@ -1,12 +1,10 @@
 package me.henry.plugin;
 
-import org.bukkit.event.EventHandler;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerBedLeaveEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.HumanEntity;
 
 public final class Plugin extends JavaPlugin implements Listener {
 
